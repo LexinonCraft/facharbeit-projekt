dccaa8e93f9251050f93cb2ced379cefc2fb4a93
LexinonCraft <willi4wuff@gmail.com>
2023-02-26 20:26:18 +0100

    Added PerlinNoiseGenerator class

---
+++ src/main/java/com/lexinon/facharbeit/PerlinNoiseGenerator.java
@@ -0,0 +1,98 @@
+package com.lexinon.facharbeit;
+
+import org.joml.Vector2f;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PerlinNoiseGenerator {
+
+    private final float[] texture;
+    private final int textureSideLength;
+    private float totalWeight;
+    private final int seed;
+    private final MessageDigest md;
+
+    public PerlinNoiseGenerator(int seed, int textureSideLength) {
+        this.seed = seed;
+        this.textureSideLength = textureSideLength;
+
+        texture = new float[textureSideLength * textureSideLength];
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Couldn't initialize hashing algorithm!");
+        }
+    }
+
+    public PerlinNoiseGenerator noise(float frequency, float weight) {
+        float gridPointDistance = textureSideLength / frequency;
+        int amountGridPoints = (int) (frequency + 2);
+
+        int gridPoints = amountGridPoints * amountGridPoints;
+        Vector2f[] gradients = new Vector2f[gridPoints];
+
+        for(int i = 0; i < gridPoints; i++) {
+            float angle = hash(i) % 36_000f / 18_000f * (float) Math.PI;
+            gradients[i] = new Vector2f((float) Math.sin(angle), (float) Math.cos(angle));
+        }
+
+        for(int x = 0; x < textureSideLength; x++) {
+            for(int y = 0; y < textureSideLength; y++) {
+                int gridPointX = (int) (x / gridPointDistance);
+                int gridPointY = (int) (y / gridPointDistance);
+                Vector2f gradient1 = gradients[getIndexByPos(gridPointX, gridPointY, amountGridPoints)];
+                Vector2f gradient2 = gradients[getIndexByPos(gridPointX + 1, gridPointY, amountGridPoints)];
+                Vector2f gradient3 = gradients[getIndexByPos(gridPointX + 1, gridPointY + 1, amountGridPoints)];
+                Vector2f gradient4 = gradients[getIndexByPos(gridPointX, gridPointY + 1, amountGridPoints)];
+                float relX = x / gridPointDistance - gridPointX;
+                float relY = y / gridPointDistance - gridPointY;
+
+                float s1 = gradient1.dot(new Vector2f(relX, relY));
+                float s2 = gradient2.dot(new Vector2f(relX - 1, relY));
+                float s3 = gradient3.dot(new Vector2f(relX - 1, relY - 1));
+                float s4 = gradient4.dot(new Vector2f(relX, relY - 1));
+
+                float f0 = s1 * interpolationFunction(1 - relX) + s2 * interpolationFunction(relX);
+                float f1 = s4 * interpolationFunction(1 - relX) + s3 * interpolationFunction(relX);
+                float fRes = f0 * interpolationFunction(1 - relY) + f1 * interpolationFunction(relY);
+
+                texture[getIndexByPos(x, y, textureSideLength)] += fRes * weight;
+            }
+        }
+        totalWeight += weight;
+        return this;
+    }
+
+    private int hashBackbone(int x) {
+        byte[] bytes = md.digest(new byte[]{(byte) ((x >> 24) & 0x000000FF), (byte) ((x >> 16) & 0x000000FF), (byte) ((x >> 8) & 0x000000FF), (byte) (x & 0x000000FF)});
+        return (bytes[0] << 24) + (bytes[1] << 16) + (bytes[2] << 8) + bytes[3];
+    }
+
+    private int hash(int x) {
+        return hashBackbone(x + hashBackbone(seed));
+    }
+
+    private float interpolationFunction(float a) {
+        return a * a * a * (6 * a * a - 15 * a + 10);
+    }
+
+    public int getIndexByPos(int x, int y, int length) {
+        return x + y * length;
+    }
+
+    private static int ceilDiv(float a, float b) {
+        return (int) Math.floor(a / b) + 1;
+    }
+
+    public void normalizeTexture() {
+        for(int i = 0; i < texture.length; i++) {
+            texture[i] /= totalWeight;
+        }
+        totalWeight = 1;
+    }
+
+    public float[] getTexture() {
+        return texture;
+    }
+}
--- src/test/java/com/lexinon/facharbeit/test/PerlinTest.java
+++ src/test/java/com/lexinon/facharbeit/test/PerlinTest.java
@@ -1,116 +1,39 @@
 package com.lexinon.facharbeit.test;
 
-import org.joml.Vector2f;
+import com.lexinon.facharbeit.PerlinNoiseGenerator;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class PerlinTest {
 
     public static void main(String[] args) throws IOException {
         int textureSideLength = 512;
 
-        float[] texture = new float[textureSideLength * textureSideLength];
-        float weight = 0;
-        weight += noise(textureSideLength, 8, 0, texture, 2f);
-        weight += noise(textureSideLength, 32, 0, texture, 1f);
+        PerlinNoiseGenerator perlinNoiseGenerator = new PerlinNoiseGenerator(0, textureSideLength);
+        perlinNoiseGenerator.noise(8f, 1f)
+                        .noise(32f, 1f)
+                        .noise(14f, 2f);
 
+        perlinNoiseGenerator.normalizeTexture();
+        float[] texture = perlinNoiseGenerator.getTexture();
         for(int x = 0; x < textureSideLength; x++) {
             for(int y = 0; y < textureSideLength; y++) {
-                texture[getIndexByPos(x, y, textureSideLength)] = texture[getIndexByPos(x, y, textureSideLength)] / weight;
+                texture[perlinNoiseGenerator.getIndexByPos(x, y, textureSideLength)] = texture[perlinNoiseGenerator.getIndexByPos(x, y, textureSideLength)];
             }
         }
 
         BufferedImage image = new BufferedImage(textureSideLength, textureSideLength, BufferedImage.TYPE_INT_ARGB);
         for(int x = 0; x < textureSideLength; x++) {
             for(int y = 0; y < textureSideLength; y++) {
-                float value = texture[getIndexByPos(x, y, textureSideLength)];
+                float value = texture[perlinNoiseGenerator.getIndexByPos(x, y, textureSideLength)];
                 image.setRGB(x, y, new Color(value / 2 + 0.5f, value / 2 + 0.5f, value / 2 + 0.5f).getRGB());
             }
         }
         ImageIO.write(image, "png", new File("perlin.png"));
-    }
-
-    public static float noise(int textureSideLength, float frequency, int seed, float[] texture, float weight) {
-        float gridPointDistance = 1f / frequency * textureSideLength;
-        int amountGridPoints = ceilDiv(textureSideLength, gridPointDistance) + 1;
-
-        int gridPoints = amountGridPoints * amountGridPoints;
-        Vector2f[] gradients = new Vector2f[gridPoints];
-
-        MessageDigest md = null;
-        try {
-            md = MessageDigest.getInstance("SHA256");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
-        for(int i = 0; i < gridPoints; i++) {
-            float angle = hash(i, seed, md) % 36_000f / 18_000f * (float) Math.PI;
-            gradients[i] = new Vector2f((float) Math.sin(angle), (float) Math.cos(angle));
-        }
-
-        for(int x = 0; x < textureSideLength; x++) {
-            for(int y = 0; y < textureSideLength; y++) {
-                int gridPointX = x / (int) gridPointDistance;
-                int gridPointY = y / (int) gridPointDistance;
-                Vector2f gradient1 = gradients[getIndexByPos(gridPointX, gridPointY, amountGridPoints)];
-                Vector2f gradient2 = gradients[getIndexByPos(gridPointX + 1, gridPointY, amountGridPoints)];
-                Vector2f gradient3 = gradients[getIndexByPos(gridPointX + 1, gridPointY + 1, amountGridPoints)];
-                Vector2f gradient4 = gradients[getIndexByPos(gridPointX, gridPointY + 1, amountGridPoints)];
-                float relX = x % gridPointDistance / (float) gridPointDistance;
-                float relY = y % gridPointDistance / (float) gridPointDistance;
-
-                float s1 = gradient1.dot(new Vector2f(relX, relY));
-                float s2 = gradient2.dot(new Vector2f(relX - 1, relY));
-                float s3 = gradient3.dot(new Vector2f(relX - 1, relY - 1));
-                float s4 = gradient4.dot(new Vector2f(relX, relY - 1));
-
-                float f0 = s1 * interpolationFunction(1 - relX) + s2 * interpolationFunction(relX);
-                float f1 = s4 * interpolationFunction(1 - relX) + s3 * interpolationFunction(relX);
-                float fRes = f0 * interpolationFunction(1 - relY) + f1 * interpolationFunction(relY);
-
-                texture[getIndexByPos(x, y, textureSideLength)] += fRes * weight;
-                //image.setRGB(x, y, new Color(fRes / 2 + 0.5f, fRes / 2 + 0.5f, fRes / 2 + 0.5f).getRGB());
-            }
-        }
-        return weight;
-    }
-
-    private static int getIndexByPos(int x, int y, int length) {
-        return x + y * length;
-    }
-
-    private static int getXPosByIndex(int i, int length) {
-        return i % length;
-    }
-    private static int getYPosByIndex(int i, int length) {
-        return i / length;
-    }
-
-    private static int hash(int x, MessageDigest md) {
-        byte[] bytes = md.digest(new byte[]{(byte) ((x >> 24) & 0x000000FF), (byte) ((x >> 16) & 0x000000FF), (byte) ((x >> 8) & 0x000000FF), (byte) (x & 0x000000FF)});
-        return (bytes[0] << 24) + (bytes[1] << 16) + (bytes[2] << 8) + bytes[3];
-    }
-
-    private static int hash(int x, int seed, MessageDigest md) {
-        return hash(x + hash(seed, md), md);
-    }
-
-    private static float interpolationFunction(float a) {
-        return a * a * a * (6 * a * a - 15 * a + 10);
-        //return 3 * a * a - 2 * a * a * a;
-        //return  a;
-    }
-
-    private static int ceilDiv(float a, float b) {
-        return (int) Math.floor(a / b) + 1;
     }
 
 }
